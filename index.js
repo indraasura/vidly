4c690647c5527366c6d6c6c783b2bab23c9e5fee
Swarup Hegde <swaruphegde@gmail.com>
2020-03-14 22:43:32 +0530

    feat: endpoints for put and deleting genres

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const Joi = require('joi')
 app.use(express.json())
 
 const genres = [
-    {id: 1, name: "horror"},
-    {id: 2, name: "comedy"},
-    {id: 3, name: "romance"},
-    {id: 4, name: "drama"},
-    {id: 5, name: "sci-fi"},
+    {id: 1, genre: "horror"},
+    {id: 2, genre: "comedy"},
+    {id: 3, genre: "romance"},
+    {id: 4, genre: "drama"},
+    {id: 5, genre: "sci-fi"},
 ]
 
 // Get routes
@@ -29,18 +29,47 @@ app.get('/api/genres/:id', (req, res) => {
 // Post routes
 app.post('/api/genres', (req, res) => {
     const schema = {
-        name: Joi.string().min(3).required()
+        genre: Joi.string().min(3).required()
     }
-    const {error} = Joi.validate(req.body.name, schema)
+    const {error} = validateGenre(req.body)
     if (error) return res.status(400).send(error.details[0].message)
     const genre = {
         id: genres.length + 1,
-        name: req.body.name
+        genre: req.body.genre
     }
     genres.push(genre)
     res.send(genres)
 })
 
+// Put routes
+app.put('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id == parseInt(req.params.id))
+    if(!genre) return res.status(404).send('Genre not found')
+    const schema = {
+        genre: Joi.string().min(3).required()
+    }
+    const {error} = validateGenre(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+    genre.genre = req.body.genre
+    res.send(genre)
+})
+
+// Delete routes
+app.delete('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id == parseInt(req.params.id))
+    if(!genre) return res.status(404).send('Genre not found')
+    const index = genres.indexOf(genre)
+    genres.splice(index, 1)
+    res.send(genres)
+})
+
+
+validateGenre = genre => {
+    const schema = {
+        genre: Joi.string().min(3).required()
+    }
+    return Joi.validate(genre, schema)
+}
 
 
 app.listen(3000, ()=>console.log('Listening on port 3000...'))
